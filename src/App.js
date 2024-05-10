150769fa3124b36f6d147df0a56d86ba7a239a86
nardi22 <162893687+nardi22@users.noreply.github.com>
2024-05-10 20:28:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Movie from './components/Movie';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render */}
+      <Home />
+      {/* <Movie /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import Banner from '../../components/Banner/Banner';
+import RowList from '../../components/Row/RowList/RowList';
+
+const Home = () => {
+  return (
+    // Fragment
+    <>
+    <Header />
+    <Banner />
+    <RowList />
+    <Footer />
+      
+    </>
+  )
+}
+
+export default Home
